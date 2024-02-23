0255af7325083b7e262e0d0a5b0e495dee35a296
Federico Torres <fede@readyset.co>
2024-02-23 18:56:35 +0100

    2nd draft

--- Desafio 1/main.js
+++ Desafio 1/main.js
@@ -1,19 +1,29 @@
 // CoderHouse - Curso de Backend
 // Desafío #1
 // Alumno: Federico Torres
-
+// Fecha límite entrega 29/Feb 23:59
 
 class ProductManager {
+    static id = 0;
     constructor (products){
         this.products = [];
     }
 
     addProduct(title,description,price,thumbnail,code,stock){
         // id incrementable
+        ProductManager.id++;
 
+        
         // validar que code no se repita
         
+
         // validar que estén todos los campos
+        
+        // agregar item al array
+        this.products.push([id,title,description,price,thumbnail,code,stock]);
+
+        //confirmar por consola
+        console.log("Producto agregado con éxito: "+id+" - "+title);
     }
 
     getProduct(){
@@ -32,12 +42,12 @@ class ProductManager {
 
 
 
+// TESTING
 
+const test = new ProductManager();
 
-// elementos de products:
-// title
-// description
-// price
-// thumbnail
-// code
-// stock
+console.log(test.getProduct());
+
+test.addProduct("producto prueba","Este es un producto de prueba",200,"Sin imagen","abc123",25);
+
+console.log(test.getProduct());
