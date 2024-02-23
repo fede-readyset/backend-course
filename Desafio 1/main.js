64d859ee48b01db116233311cd044bb15313daba
Federico Torres <fede@readyset.co>
2024-02-23 18:28:29 +0100

    First Draft

--- Desafio 1/main.js
+++ Desafio 1/main.js
@@ -1,1 +1,43 @@
-console.log("Hola Mundo");
+// CoderHouse - Curso de Backend
+// Desafío #1
+// Alumno: Federico Torres
+
+
+class ProductManager {
+    constructor (products){
+        this.products = [];
+    }
+
+    addProduct(title,description,price,thumbnail,code,stock){
+        // id incrementable
+
+        // validar que code no se repita
+        
+        // validar que estén todos los campos
+    }
+
+    getProduct(){
+        //devolver array con todos los productos
+        return this.products;
+    }
+
+    getProductById(){
+        // devolver array del producto buscado
+        // buscar producto
+        // si no existe, mostrar "Not Found"
+    }
+
+}
+
+
+
+
+
+
+// elementos de products:
+// title
+// description
+// price
+// thumbnail
+// code
+// stock
