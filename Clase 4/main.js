ef645e120e938cc8564e8a05041c39e1ca4e8b16
Federico Torres <fede@readyset.co>
2024-02-29 14:18:45 +0100

    Commit

---
+++ Clase 4/main.js
@@ -0,0 +1,12 @@
+/** CLASE 4 MANEJO DE ARCHIVOS */
+
+// Temas de hoy:
+
+// 1) Filesystem
+// 2) Manejo de archivos de forma sincrónica
+// 3) Manejo de archivos con callbacks
+// 4) Manejo de archivos con promesas
+// 5) Manejo de datos complejos
+// 6) Presentación de Desafío #2
+
+
