917363e33b05da09a9364bd4b40a3d251d4367c4
Federico Torres <fede@readyset.co>
2024-03-08 16:33:40 +0100

    Desafío 3 - Listo

--- Desafio 3/src/ProductManager.js
+++ Desafio 3/src/ProductManager.js
@@ -24,7 +24,7 @@ export class ProductManager {
         return maxId;
     }
 
-    getProduct(callback) {
+/*     getProduct(callback) {
         fs.readFile(this.path, "utf-8", (error, contenido) => {
             if (error) {
                 // Chequear si el archivo no existe o está vacío
@@ -40,10 +40,32 @@ export class ProductManager {
                 callback(parseError); // Return parsing errors
             }
         });       
+    } */
+
+    getProduct() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, "utf-8", (error, contenido) => {
+                if (error) {
+                    // Chequear si el archivo no existe o está vacío
+                    if (error.code === 'ENOENT' || contenido.trim() === '') {
+                        resolve([]); // Devolver array vacío
+                    } else {
+                        reject(error); // Devolver otros errores
+                    }
+                } else {
+                    try {
+                        const products = JSON.parse(contenido);
+                        resolve(products);
+                    } catch (parseError) {
+                        reject(parseError); // Return parsing errors
+                    }
+                }
+            });
+        });
     }
 
 
-    addProduct(product,callback){
+ /*    addProduct(product,callback){
         this.getProduct((error,existingProducts=[]) => {
 
             if(!error){ 
@@ -72,10 +94,34 @@ export class ProductManager {
                 callback(null,this.id);
             });
         });
+    } */
+
+    addProduct(product) {
+        return new Promise((resolve, reject) => {
+            this.getProduct()
+                .then(existingProducts => {
+                    if (existingProducts.some(existingProduct => existingProduct.code === product.code)) {
+                        throw new Error("ERROR. No se puede agregar el producto porque el código " + product.code + " ya existe.");
+                    }
+                    this.id = this.findHighestId(existingProducts) + 1;
+                    const requiredFields = ['name', 'code', 'description', 'thumbnail', 'price', 'stock'];
+                    const missingFields = requiredFields.filter(field => !product[field]);
+                    if (missingFields.length > 0) {
+                        throw new Error("ERROR. No se puede agregar el producto porque faltan los siguientes campos: " + missingFields.join(', '));
+                    }
+                    product.id = this.id;
+                    existingProducts.push(product);
+                    fs.writeFile(this.path, JSON.stringify(existingProducts, null, 2), error => {
+                        if (error) reject(error);
+                        resolve(this.id);
+                    });
+                })
+                .catch(error => reject(error));
+        });
     }
 
 
-    getProductById(id) {
+/*     getProductById(id) {
         return new Promise((resolve, reject) => {
             this.getProduct((error, existingProducts = []) => {
                 if (error) {
@@ -86,9 +132,19 @@ export class ProductManager {
                 }
             });
         });
+    } */
+    getProductById(id) {
+        return new Promise((resolve, reject) => {
+            this.getProduct()
+                .then(existingProducts => {
+                    const buscado = existingProducts.find(objeto => objeto.id === id);
+                    buscado ? resolve(buscado) : reject({ error: `No se encontró ningún producto con id ${id}.` });
+                })
+                .catch(error => reject(error));
+        });
     }
 
-    updateProduct(id,product,callback) {
+/*     updateProduct(id,product,callback) {
         this.getProduct((error,existingProducts=[]) => {
 
             if(!error){ 
@@ -116,10 +172,35 @@ export class ProductManager {
                 callback(null,this.id);
             });
         });
+    } */
+    updateProduct(id, product) {
+        return new Promise((resolve, reject) => {
+            this.getProduct()
+                .then(existingProducts => {
+                    if (existingProducts.some(existingProduct => existingProduct.code === product.code && existingProduct.id !== id)) {
+                        throw new Error("ERROR. No se puede actualizar el producto porque el código " + product.code + " ya existe.");
+                    }
+                    const requiredFields = ['name', 'code', 'description', 'thumbnail', 'price', 'stock'];
+                    const missingFields = requiredFields.filter(field => !product[field]);
+                    if (missingFields.length > 0) {
+                        throw new Error("ERROR. No se puede actualizar el producto porque faltan los siguientes campos: " + missingFields.join(', '));
+                    }
+                    const index = existingProducts.findIndex(product => product.id === id);
+                    if (index !== -1) {
+                        product.id = id;
+                        existingProducts[index] = { ...product };
+                    }
+                    fs.writeFile(this.path, JSON.stringify(existingProducts, null, 2), error => {
+                        if (error) reject(error);
+                        resolve(this.id);
+                    });
+                })
+                .catch(error => reject(error));
+        });
     }
 
 
-    deleteProduct(id) {
+    /* deleteProduct(id) {
         return new Promise((resolve, reject) => {
             this.getProduct((error, existingProducts = []) => {
                 if (error) {
@@ -144,7 +225,28 @@ export class ProductManager {
             });
         });
     }
-
+ */
+    deleteProduct(id) {
+        return new Promise((resolve, reject) => {
+            this.getProduct()
+                .then(existingProducts => {
+                    const index = existingProducts.findIndex(producto => producto.id === id);
+                    if (index !== -1) {
+                        existingProducts.splice(index, 1);
+                        fs.writeFile(this.path, JSON.stringify(existingProducts, null, 2), error => {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                resolve({ message: `El producto con id ${id} ha sido eliminado correctamente.` });
+                            }
+                        });
+                    } else {
+                        reject({ error: `No se encontró ningún producto con id ${id}.` });
+                    }
+                })
+                .catch(error => reject(error));
+        });
+    }
 }
 
 
@@ -171,10 +273,10 @@ const test = new ProductManager("./db.json");
 
 
 // Defino algunos objetos product de ejemplo
- let product1 = { name: "Producto 1", description: "1111", price: 200, thumbnail: "Sin imagen", code: "prod1", stock:100 };
- let product2 = { name: "Producto 2", description: "2222", price: 200, thumbnail: "Sin imagen", code: "prod2", stock:200 };
- let product3 = { name: "Producto 3", description: "3333", price: 200, thumbnail: "Sin imagen", code: "prod3", stock:300 };
- let product4 = { name: "Producto 4", description: "4444", price: 200, thumbnail: "Sin imagen", code: "prod4", stock:400 };
+//  let product1 = { name: "Producto 1", description: "1111", price: 200, thumbnail: "Sin imagen", code: "prod1", stock:100 };
+//  let product2 = { name: "Producto 2", description: "2222", price: 200, thumbnail: "Sin imagen", code: "prod2", stock:200 };
+//  let product3 = { name: "Producto 3", description: "3333", price: 200, thumbnail: "Sin imagen", code: "prod3", stock:300 };
+//  let product4 = { name: "Producto 4", description: "4444", price: 200, thumbnail: "Sin imagen", code: "prod4", stock:400 };
 
 
 // Agrego productos al archivo mediante addProduct
--- Desafio 3/src/app.js
+++ Desafio 3/src/app.js
@@ -1,31 +1,59 @@
-
-// Importamos el módulo ProductManager.js: 
+// Importo el módulo propio ProductManager.js y el módulo de terceros express  
 import { ProductManager } from "./ProductManager.js";
 import express from "express";
 
-
+// Defino algunas variables e instancio clases
 const PUERTO = 8080;
 const app = express();
 const PM = new ProductManager("./db.json");
 
-
+// Primer endpoint con limit optativo por query
 app.get ("/products", (req,res) => {
     let limit = parseInt(req.query.limit);
-    console.log(limit);
-    if (!limit){
-        PM.getProduct( (error,products) => {
-            res.send("<pre>"+JSON.stringify(products,null,2)+"</pre>");
+
+    PM.getProduct()
+        .then (products => {
+            if (!limit){
+                res.send(formatDataAsTable(products));
+            }
+            else {
+                res.send(formatDataAsTable(products, limit));
+            }
         })
-    }
-    else{
-        PM.getProduct( (error,products) => {
-            res.send("<pre>"+JSON.stringify(products.slice(0,limit),null,2)+"</pre>");
-        })
-    }
-    
+        .catch (error => res.send(error));
 })
 
+// Segundo endpoint con pid por params 
+app.get ("/products/:pid", (req,res) => {
+    let pid = parseInt(req.params.pid);
+    PM.getProductById(pid)
+        .then (product => res.send(formatDataAsTable([product])))
+        .catch (error => res.send(error));
+});
+
+// Listener
 app.listen(PUERTO, () => {
     console.log(`Escuchando en el http://localhost:${PUERTO}`);
 });
 
+
+// Función opcional para dar formato de tabla HTML al objeto
+function formatDataAsTable(data, limit) {
+    let html = '<table style="border-collapse: collapse; width: 100%;">';
+    html += '<tr style="border: 1px solid #dddddd; text-align: left; padding: 8px; background-color: #f2f2f2;">';
+    for (const key in data[0]) {
+        html += `<th>${key}</th>`;
+    }
+    html += '</tr>';
+
+    data.slice(0, limit).forEach(item => {
+        html += '<tr>';
+        for (const key in item) {
+            html += `<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item[key]}</td>`;
+        }
+        html += '</tr>';
+    });
+
+    html += '</table>';
+    return html;
+}
